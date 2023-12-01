39ea0c6ca5535449d03590c095ca331ace75df59
Wojciech <klonowski.wojtek@yahoo.co.uk>
2023-12-01 15:12:11 +0100

    final

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -24,4 +24,26 @@ class PolyTreeNode
         child_node.parent = nil
         raise 'not a child' if !self.children.include?(child_node)
     end
+
+    def dfs(target_value)
+        return self if value == target_value
+        children.each do |child|
+            result = child.dfs(target_value)
+            return result unless result.nil?
+        end
+        nil
+    end
+
+    def bfs(target_value)
+
+        queue = [self]
+
+        until queue.empty?
+            current_node = queue.shift
+            return current_node if current_node.value == target_value
+
+            queue.concat(current_node.children)
+        end
+        nil
+    end
 end
